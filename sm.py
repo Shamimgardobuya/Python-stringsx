1265bb058dcab3a70aab52bb632021561e4d5e9f
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-01 15:31:06 +0300

    strings exercise

---
+++ sm.py
@@ -0,0 +1,35 @@
+ python3
+Python 3.8.10 (default, Mar 15 2022, 12:22:08) 
+[GCC 9.4.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> name="John Smith"
+>>> print(name[1])
+o
+>>> print(name[2])
+h
+>>> print(name[-2])
+t
+>>> print(name[1:-1])
+ohn Smit
+>>> len(name)
+10
+>>> print(f"{2+2}+{10%3}")
+4+1
+>>> name="John Smith"
+>>> print(name.title())
+John Smith
+>>> name="john smith"
+>>> print(name.title())
+John Smith
+>>> print(name.strip())
+john smith
+>>> print(name.lstrip())
+john smith
+>>> print(name.find("Smith"))
+-1
+>>> print(name.replace("j","k"))
+kohn smith
+
+
+
+
